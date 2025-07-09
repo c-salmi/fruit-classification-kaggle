8cd3280b70ad72bcfda02d5b431ec4581fe1e8c6
Chadi Salmi <salmi.chadi@gmail.com>
2025-07-09 19:41:14 +0200

    fix devices

--- src/main.py
+++ src/main.py
@@ -21,7 +21,13 @@ def main():
         use_augmentation=use_augmentation,
         augmentation_strength=augmentation_strength,
     )
-    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    device = torch.device(
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
     print(f"Using device: {device}")
 
     mlflow.set_tracking_uri("http://localhost:8080")
